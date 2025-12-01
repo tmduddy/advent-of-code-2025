ef248005bf2200e3a9519d588a705888a00d63cb
tmduddy <login@tylerduddy.com>
2025-12-01 12:46:47 -0500

    setup for day1

---
+++ days/day1/index.ts
@@ -0,0 +1,23 @@
+import {readFileSync} from 'fs';
+import path from 'path';
+
+const inputFileName =
+  process.env.AOC_DEMO === 'true' ? './demo.txt' : './input.txt';
+const input = readFileSync(path.resolve(__dirname, inputFileName), {
+  encoding: 'utf8',
+  flag: 'r',
+}).split('\n');
+console.log(input);
+
+const part1 = () => {
+  const solution = 0;
+  console.log(`\nPart 1a: ${solution}`);
+};
+
+const part2 = () => {
+  const solution = 0;
+  console.log(`\nPart 2: ${solution}`);
+};
+
+part1();
+part2();
