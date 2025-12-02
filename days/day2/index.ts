555ea5baca35c22424ade416bde23fb3755c0c60
tmduddy <login@tylerduddy.com>
2025-12-02 14:09:52 -0500

    day2 part2 brute force

--- days/day2/index.ts
+++ days/day2/index.ts
@@ -1,8 +1,8 @@
 import {readFileSync} from 'fs';
-import * as _ from 'lodash'
+import * as _ from 'lodash';
 import path from 'path';
 
-import { debugLog } from '../utils/utils';
+import {debugLog} from '../utils/utils';
 
 const inputFileName =
   process.env.AOC_DEMO === 'true' ? './demo.txt' : './input.txt';
@@ -14,14 +14,14 @@ debugLog(input);
 
 const isInvalid = (input: string) => {
   const length = input.length;
-  if (length % 2 != 0) {
+  if (length % 2 !== 0) {
     return false;
   }
   // split in half
   const half = length / 2;
   const front = input.substring(0, half);
   const back = input.substring(half);
-  
+
   // compare halves
   return front === back;
 };
@@ -29,12 +29,10 @@ const isInvalid = (input: string) => {
 const part1 = () => {
   const ids = input[0].split(',');
   debugLog(ids);
-  const invalidIds: string[] = [];
+  const invalidIds: number[] = [];
   ids.forEach(range => {
     const [first, last] = range.split('-').map(Number);
-    const allIds = _.range(first, last+1).map(i => i.toString());
-    const newInvalid = allIds.filter(isInvalid);
-    debugLog(newInvalid);
+    const allIds = _.range(first, last + 1).map(i => i.toString());
     invalidIds.push(...allIds.filter(isInvalid).map(Number));
   });
 
@@ -44,8 +42,41 @@ const part1 = () => {
   console.log(`\nPart 1: ${solution}`);
 };
 
+const isInvalidPart2 = (input: string, range: string) => {
+  const length = input.length;
+  if (length === 1) {
+    return false;
+  }
+  const half = Math.ceil(length / 2);
+  let isInvalid = false;
+  for (let i = 1; i <= half; i++) {
+    const sub = input.substring(0, i);
+    const replaced = input.replaceAll(sub, '');
+    isInvalid = replaced === '';
+    if (isInvalid) {
+      debugLog(`--- ${sub} is invalid for ${input} in range ${range} ---`);
+      break;
+    }
+  }
+
+  return isInvalid;
+};
+
 const part2 = () => {
-  const solution = 0;
+  const ids = input[0].split(',');
+  debugLog(ids);
+  const invalidIds: number[] = [];
+  ids.forEach(range => {
+    const [first, last] = range.split('-').map(Number);
+    const allIds = _.range(first, last + 1).map(i => i.toString());
+    invalidIds.push(
+      ...allIds.filter(i => isInvalidPart2(i, range)).map(Number),
+    );
+  });
+
+  debugLog(invalidIds);
+
+  const solution = invalidIds.reduce((a, c) => a + c, 0);
   console.log(`\nPart 2: ${solution}`);
 };
 
