df7b0982e37ed9296110bb49f38448c081ae6fa0
tmduddy <login@tylerduddy.com>
2025-12-02 14:19:49 -0500

    add timer

--- days/utils/utils.ts
+++ days/utils/utils.ts
@@ -1,8 +1,22 @@
 /**
  * console log that only applies when in demo or debug mode to keep prod solutions clean
  */
-export const debugLog = (...args) => {
+export const debugLog = (...args: any[]) => {
   if (process.env.AOC_DEMO === 'true' || process.env.AOC_DEBUG === 'true') {
     console.log(...args);
   }
 };
+
+/**
+ * print out timing information
+ */
+export const timeIt = (fn: ()=>void) => {
+  if (process.env.AOC_TIME === 'true') {
+    const start = performance.now();
+    fn();
+    const end = performance.now();
+    console.log(`execution took ${end - start} ms`);
+  } else {
+    fn();
+  }
+}
