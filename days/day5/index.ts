3cdf5aab0a08c8a6ef6e2f6d07ab94b6eb536b39
tmduddy <login@tylerduddy.com>
2025-12-05 15:16:29 -0500

    lint

--- days/day5/index.ts
+++ days/day5/index.ts
@@ -39,48 +39,72 @@ const part1 = () => {
   console.log(`\nPart 1: ${solution}`);
 };
 
-const addComparisons = (comparisons: Set<string>, rIdx: number, compIdx: number) => {
+const addComparisons = (
+  comparisons: Set<string>,
+  rIdx: number,
+  compIdx: number,
+) => {
   comparisons.add(`${rIdx}_${compIdx}`);
   comparisons.add(`${compIdx}_${rIdx}`);
-}
+};
 
 const part2 = () => {
   const lineBreak = input.indexOf('');
   const ranges = input.slice(0, lineBreak);
 
-  let allRanges = ranges.map(r => r.split('-').map(Number));
+  const allRanges = ranges.map(r => r.split('-').map(Number));
 
   allRanges.forEach((range, rIdx) => {
     debugLog(`R_${rIdx}: ${range.join('-')}`);
   });
 
-  let comparisons: Set<string>= new Set();
-  let freshRanges: Set<string> = new Set();
+  const comparisons: Set<string> = new Set();
+  const freshRanges: Set<string> = new Set();
 
   allRanges.forEach((range, rIdx) => {
     let [freshStart, freshEnd] = structuredClone(range);
     debugLog(`R_${rIdx}: ${range.join('-')}`);
     allRanges.forEach((compRange, compIdx) => {
       const [compStart, compEnd] = compRange;
-      if (rIdx !== compIdx) { // && !comparisons.has(`${rIdx}_${compIdx}`)){
+      if (rIdx !== compIdx) {
+        // && !comparisons.has(`${rIdx}_${compIdx}`)){
         addComparisons(comparisons, rIdx, compIdx);
         // debugLog(`starting comparison for R_${rIdx} against C_${compIdx}`);
         // if my end is in your range but my start is not:
-        if (freshEnd >= compStart && freshEnd <= compEnd && freshStart < compStart) {
+        if (
+          freshEnd >= compStart &&
+          freshEnd <= compEnd &&
+          freshStart < compStart
+        ) {
           freshEnd = compStart - 1;
-          debugLog(`\tend is in C_${compIdx} ${compRange.join('-')}, new safe end is ${freshEnd}`);
+          debugLog(
+            `\tend is in C_${compIdx} ${compRange.join('-')}, new safe end is ${freshEnd}`,
+          );
         }
         // if my start is in your range but my end is not:
-        if (freshStart >= compStart && freshStart <= compEnd && freshEnd > compEnd) { 
+        if (
+          freshStart >= compStart &&
+          freshStart <= compEnd &&
+          freshEnd > compEnd
+        ) {
           freshStart = compEnd + 1;
-          debugLog(`\tstart is in C_${compIdx} ${compRange.join('-')}, new safe start is ${freshStart}`);
+          debugLog(
+            `\tstart is in C_${compIdx} ${compRange.join('-')}, new safe start is ${freshStart}`,
+          );
         }
         // if my range is entirely within your range:
-        if (freshStart >= compStart && freshStart <=compEnd && freshEnd <= compEnd && freshEnd >= compStart) {
+        if (
+          freshStart >= compStart &&
+          freshStart <= compEnd &&
+          freshEnd <= compEnd &&
+          freshEnd >= compStart
+        ) {
           freshStart = 0;
           freshEnd = 0;
-          debugLog(`\trange is completely within C_${compIdx} ${compRange.join('-')}, no fresh ingredients`);
-        } 
+          debugLog(
+            `\trange is completely within C_${compIdx} ${compRange.join('-')}, no fresh ingredients`,
+          );
+        }
       }
     });
     debugLog(`final fresh range for R_${rIdx} is ${freshStart}-${freshEnd}`);
@@ -88,17 +112,17 @@ const part2 = () => {
     if (freshStart > freshEnd) {
       debugLog(`R_${rIdx} is fully accounted for in other ranges.`);
     } else {
-      freshRanges.add(`${freshStart}-${freshEnd}`); 
+      freshRanges.add(`${freshStart}-${freshEnd}`);
     }
   });
   debugLog(freshRanges);
 
-  let fresh = [...freshRanges].reduce((numFresh, currRange) => {
+  const fresh = [...freshRanges].reduce((numFresh, currRange) => {
     const [start, end] = currRange.split('-').map(Number);
     if (start === 0 && end === 0) {
       return numFresh;
     }
-    return numFresh + 1 + (end-start);
+    return numFresh + 1 + (end - start);
   }, 0);
 
   const solution = fresh;
@@ -106,4 +130,4 @@ const part2 = () => {
 };
 
 timeIt(part1);
-timeIt(part2); 
+timeIt(part2);
